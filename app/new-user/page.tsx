64cf6208a66ab2798b25156663cb6646cde127b4
adi bangkit <adibangkai@gmail.com>
2023-07-30 20:58:10 +0700

    auth redirect

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,29 @@
-const NewUserPage = () => {
-  return <div>new user</div>
+import { prisma } from "@/utils/db" // koneksi ke db
+import { currentUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
+const createdNewUser = async () => {
+  const user = await currentUser()
+  const match = await prisma.user.findUnique({
+    // cek user
+    where: {
+      clerkId: user.id as string,
+    },
+  })
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user?.emailAddresses[0].emailAddress,
+      },
+    })
+  }
+  redirect("/journal")
+}
+
+const NewUserPage = async () => {
+  await createdNewUser()
+  return <div>...LOADING</div>
 }
 
 export default NewUserPage
